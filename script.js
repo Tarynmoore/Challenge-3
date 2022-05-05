eadff9691f43f686a8661303f98c7436c46513c3
Taryn Moore <tarynmoore628@gmail.com>
2022-05-05 15:57:35 -0600

    Added comments on the javascript file.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// All options for password. 
 var optionsNumbers = "0123456789";
 var optionsLowercase = "abcdefghijklmnopqrstuvwxyz";
 var optionsUppercase = "ABCDEFGHIJKJLMNOPQRSTUVWXYZ";
@@ -8,7 +9,7 @@ var finalPassword = "";
 
 
 var generatePassword = function () {
-
+  //Sets the character limit 
   var userCharacter = window.prompt("How many characters do you want in your password? \nMin: " + characterMin + " Max: " + characterMax);
   if (userCharacter < characterMin || userCharacter > characterMax) {
     window.alert("Character Min: " + characterMin + " Character Max: " + characterMax)
@@ -16,34 +17,34 @@ var generatePassword = function () {
   }
 
   console.log(userCharacter);
- 
+  // Asking users what critera wanted for password. 
   var userUppChoice = window.confirm("Do you want your password to have UPPERCASE letters?");
   var userLowChoice = window.confirm("Do you want your password to have lowercase letters?");
   var userNumbers = window.confirm("Do you want your password to have numbers?");
   var userSpecialChar = window.confirm("Do you want your password to have special characters?");
   
-  
+  // Creates the loop for password only if they are true. 
   for (var i = 0; i < userCharacter; i++) {
 
-
+    //Picks random uppercase letters from global scope. 
     if (userUppChoice === true && finalPassword.length < userCharacter) {
       var indexUpp = Math.floor(Math.random() * optionsUppercase.length)
       finalPassword = finalPassword + optionsUppercase[indexUpp];
     }
 
-
+    // Picks random lower case from global scope. 
     if (userLowChoice === true && finalPassword.length < userCharacter) {
       var indexLow = Math.floor(Math.random() * optionsLowercase.length)
       finalPassword = finalPassword + optionsLowercase[indexLow];
     }
 
-
+    // Picks random numbers from global scope. 
     if (userNumbers === true && finalPassword.length < userCharacter) {
       var indexNumbers = Math.floor(Math.random() * optionsNumbers.length)
       finalPassword = finalPassword + optionsNumbers[indexNumbers];
     }
 
-
+    // Picks random special character from global scope. 
     if (userSpecialChar === true && finalPassword.length < userCharacter) {
       var indexSpecialChar = Math.floor(Math.random() * specialCharacter.length)
       finalPassword = finalPassword + specialCharacter[indexSpecialChar];
